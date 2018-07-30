7e8c148a5c39e731ee7ccdfb929ec4da36412182
Paul Schwan <paulschwan@pauls-mbp.home>
2018-07-30 14:59:45 +0100

    First commit, based on the tutorial

---
+++ src/main/java/com/schwan/Comparator.java
@@ -0,0 +1,12 @@
+package com.schwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comparator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comparator.class, args);
+    }
+}
